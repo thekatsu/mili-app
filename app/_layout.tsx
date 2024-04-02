f88552ff97fdd072117fc34bf4a248b5ae1417e1
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2024-04-02 00:01:04 -0300

    configuração inicial concluida

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,11 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
-import { useColorScheme } from "@/components/useColorScheme";
-import "@/assets/global.css";
+import { useColorScheme } from "nativewind";
+import "@/components/styles/global.css";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -50,14 +45,14 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  const { colorScheme, setColorScheme } = useColorScheme();
+
+  setColorScheme("dark");
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+    </Stack>
   );
 }
--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,6 @@ const { withNativeWind } = require("nativewind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./assets/global.css" });
+module.exports = withNativeWind(config, {
+  input: "./components/styles/global.css",
+});
